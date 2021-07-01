75cc7d902e81abf6c44f3df0f68a25ff90b1e8f2
Semyon Mukhlaev <semen.mukhlaev@raiffeisen.ru>
2021-07-01 11:32:26 +0300

    Fix generator, fix timeout

--- client/src/Api.js
+++ client/src/Api.js
@@ -4,23 +4,10 @@ const SERVER_URL = 'http://localhost:9000';
 
 const instance = axios.create({
   baseURL: SERVER_URL,
-  timeout: 1000
+  timeout: 100000
 });
 
 export default {
-  // (C)reate
-  createNew: (text, completed) => instance.post('todos', {title: text, completed: completed}),
-  // (R)ead
-  getAll: () => instance.get('todos', {
-    transformResponse: [function (data) {
-      return data ? JSON.parse(data)._embedded.todos : data;
-    }]
-  }),
-  // (U)pdate
-  updateForId: (id, text, completed) => instance.put('todos/' + id, {title: text, completed: completed}),
-  // (D)elete
-  removeForId: (id) => instance.delete('todos/' + id),
-
   upload(file, tableName, schemaName) {
     let formData = new FormData();
 
